12ce2a8bc5f5bad6fb13d64627cc20d0d28c2ee7
Thomas Emilsson <thomasemilsson10@gmail.com>
2019-02-14 14:34:04 +0100

    updating .gitignore adding link to serverless site

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.goToGoogle = this.goToGoogle.bind(this);
+    }
+
+    goToGoogle(){
+        window.open("https://nextjs-20m4nnbnw.now.sh/", "_blank")
+    }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1> ... </h1>
+            <button type="button" className="btn btn-default" onClick={this.goToGoogle}>Test</button>
         </header>
       </div>
     );
